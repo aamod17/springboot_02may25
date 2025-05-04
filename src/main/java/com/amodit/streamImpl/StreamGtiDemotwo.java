c561dd2ac8ef009b4476e1c306159fed09a50fef
aamod17 <139898662+aamod17@users.noreply.github.com>
2025-05-04 16:56:59 +0530

    Update StreamGtiDemotwo.java
    one statement added in main() method

--- src/main/java/com/amodit/streamImpl/StreamGtiDemotwo.java
+++ src/main/java/com/amodit/streamImpl/StreamGtiDemotwo.java
@@ -1,5 +1,6 @@
 public class StreamGtiDemotwo{
   public static void main(String[] args){
     System.out.println("StreamGtiDemotwo.java");
+    System.out.println("added in main() method");
   }
 }
